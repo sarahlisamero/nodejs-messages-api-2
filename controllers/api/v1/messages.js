9d31e5368993d5305bd29cb6a03cda4db5974e71
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-11-05 17:09:16 +0100

    feat: update message

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -45,6 +45,26 @@ const create = async (req, res) => {
     });
 };
 
+const updateMessageById = async (req, res) => {
+    let id = req.params.id;
+    let message = req.body.message;
+    let m = await Message.findById(id);
+    m.message = message;
+    await m.save();
+
+    res.json({
+        status: "success",
+        message: "UPDATE a message",
+        data: [
+            {
+                message: m,
+            },
+        ],
+    });
+};
+
+
 module.exports.index = index;
 module.exports.getMessageById = getMessageById;
 module.exports.create = create;
+module.exports.updateMessageById = updateMessageById;
--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -6,8 +6,10 @@ const router = express.Router();
 // import controller
 const messagesController = require("../../../controllers/api/v1/messages");
 
-router.get("/:id", messagesController.getMessageById);
+
 router.get("/", messagesController.index);
+router.get("/:id", messagesController.getMessageById);
 router.post("/", messagesController.create);
+router.put("/:id", messagesController.updateMessageById);
 
 module.exports = router;
